a459801a1a150d7c48e278eabccb933e5c7c4a37
DowROCK <108042992+DowROCK@users.noreply.github.com>
2023-03-07 03:03:09 +0200

    Create ds.py

---
+++ ds.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+# Load a multilingual dataset (here, the Tatoeba dataset)
+df = pd.read_csv('https://object.pouta.csc.fi/Tatoeba-Challenge-2017/eng_rus_fra_deu_ita_spa_por_ell.csv')
+sentences = df['eng'].astype(str).tolist()
+
+# Define the tokenizer (converts words to integers)
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(sentences)
+
+# Convert the sentences to sequences of integers
+sequences = tokenizer.texts_to_sequences(sentences)
+
+# Pad the sequences to a fixed length
+max_length = 10
+padded_sequences = pad_sequences(sequences, maxlen=max_length, padding='pre')
+
+# Convert the padded sequences to one-hot vectors
+one_hot_sequences = to_categorical(padded_sequences, num_classes=len(tokenizer.word_index)+1)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(one_hot_sequences[:,:-1], one_hot_sequences[:,-1], test_size=0.2)
+
+# Define the model architecture
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Embedding(input_dim=len(tokenizer.word_index)+1, output_dim=64, input_length=max_length-1),
+  tf.keras.layers.LSTM(64),
+  tf.keras.layers.Dense(len(tokenizer.word_index)+1, activation='softmax')
+])
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=50)
+
+# Evaluate the model on the testing set
+loss, accuracy = model.evaluate(X_test, y_test)
+print('Test accuracy:', accuracy)
+
+# Use the model to predict the next word in a sentence
+test_sentence = 'I like to'
+test_sequence = tokenizer.texts_to_sequences([test_sentence])
+test_padded_sequence = pad_sequences(test_sequence, maxlen=max_length-1, padding='pre')
+prediction = model.predict(test_padded_sequence)
+
+# Convert the predicted integer back to a word
+predicted_word_index = tf.argmax(prediction, axis=1).numpy()[0]
+predicted_word = list(tokenizer.word_index.keys())[list(tokenizer.word_index.values()).index(predicted_word_index)]
+
+# Print the predicted word
+print(predicted_word)
